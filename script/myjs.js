4b1c41aa882f0f713195784009d32182344c0b89
hanaho fujikawa <hanagreenms06@gmail.com>
2021-02-22 21:13:23 +0900

    メッセージの追加処理実装

--- script/myjs.js
+++ script/myjs.js
@@ -9,127 +9,6 @@ axios.interceptors.request.use(request => {
   })
 
 var myId = 1
-var myIcon = 'my-icon'
-var myIconBackground = 'green'
-
-var requestApinum = 0;
-var num=0;
-var sec=1000;   
-
-var userData = [
-    {
-        id: myId,
-        name: '自分',
-        description: 'わー',
-        icon: myIcon,
-        iconBackground: myIconBackground
-    },
-   {
-       id: 1,
-       name: 'ほわん',
-        description: `　こんな夢を見た。
-        　腕組をして枕元に坐っていると、仰向に寝た女が、静かな声でもう死にますと云う。女は長い髪を枕に敷いて、輪郭の柔らかな瓜実顔をその中に横たえている。真白な頬の底に温かい血の色がほどよく差して、唇の色は無論赤い。とうてい死にそうには見えない。しかし女は静かな声で、もう死にますと判然云った。自分も確にこれは死ぬなと思った。そこで、そうかね、もう死ぬのかね、と上から覗き込むようにして聞いて見た。死にますとも、と云いながら、女はぱっちりと眼を開あけた。大きな潤のある眼で、長い睫に包まれた中は、ただ一面に真黒であった。その真黒な眸の奥に、自分の姿が鮮に浮かんでいる。
-        　自分は透き徹るほど深く見えるこの黒眼の色沢を眺めて、これでも死ぬのかと思った。それで、ねんごろに枕の傍へ口を付けて、死ぬんじゃなかろうね、大丈夫だろうね、とまた聞き返した。すると女は黒い眼を眠そうにみはったまま、やっぱり静かな声で、でも、死ぬんですもの、仕方がないわと云った。
-        　じゃ、私の顔が見えるかいと一心に聞くと、見えるかいって、そら、そこに、写ってるじゃありませんかと、にこりと笑って見せた。自分は黙って、顔を枕から離した。腕組をしながら、どうしても死ぬのかなと思った。
-        　しばらくして、女がまたこう云った。
-        「死んだら、埋めて下さい。大きな真珠貝で穴を掘って。そうして天から落ちて来る星の破片を墓標に置いて下さい。そうして墓の傍に待っていて下さい。また逢いに来ますから」
-        　自分は、いつ逢いに来るかねと聞いた。
-        「日が出るでしょう。それから日が沈むでしょう。それからまた出るでしょう、そうしてまた沈むでしょう。――赤い日が東から西へ、東から西へと落ちて行くうちに、――あなた、待っていられますか」
-        　自分は黙って首肯いた。女は静かな調子を一段張り上げて、
-        「百年待っていて下さい」と思い切った声で云った。
-        「百年、私の墓の傍に坐って待っていて下さい。きっと逢いに来ますから」
-        　自分はただ待っていると答えた。すると、黒い眸のなかに鮮に見えた自分の姿が、ぼうっと崩れて来た。静かな水が動いて写る影を乱したように、流れ出したと思ったら、女の眼がぱちりと閉じた。長い睫の間から涙が頬へ垂れた。――もう死んでいた。
-        　自分はそれから庭へ下りて、真珠貝で穴を掘った。真珠貝は大きな滑かな縁ふちの鋭どい貝であった。土をすくうたびに、貝の裏に月の光が差してきらきらした。湿った土の匂もした。穴はしばらくして掘れた。女をその中に入れた。そうして柔らかい土を、上からそっと掛けた。掛けるたびに真珠貝の裏に月の光が差した。
-        　それから星の破片の落ちたのを拾って来て、かろく土の上へ乗せた。星の破片は丸かった。長い間大空を落ちている間に、角が取れて滑かになったんだろうと思った。抱き上げて土の上へ置くうちに、自分の胸と手が少し暖くなった。
-        　自分は苔の上に坐った。これから百年の間こうして待っているんだなと考えながら、腕組をして、丸い墓石を眺めていた。そのうちに、女の云った通り日が東から出た。大きな赤い日であった。それがまた女の云った通り、やがて西へ落ちた。赤いまんまでのっと落ちて行った。一つと自分は勘定した。
-        　しばらくするとまた唐紅の天道がのそりと上って来た。そうして黙って沈んでしまった。二つとまた勘定した。
-        　自分はこう云う風に一つ二つと勘定して行くうちに、赤い日をいくつ見たか分らない。勘定しても、勘定しても、しつくせないほど赤い日が頭の上を通り越して行った。それでも百年がまだ来ない。しまいには、苔の生えた丸い石を眺めて、自分は女に欺されたのではなかろうかと思い出した。
-        　すると石の下から斜に自分の方へ向いて青い茎が伸びて来た。見る間に長くなってちょうど自分の胸のあたりまで来て留まった。と思うと、すらりと揺ぐ茎の頂に、心持首を傾けていた細長い一輪の蕾が、ふっくらと弁を開いた。真白な百合が鼻の先で骨に徹えるほど匂った。そこへ遥の上から、ぽたりと露が落ちたので、花は自分の重みでふらふらと動いた。自分は首を前へ出して冷たい露の滴る、白い花弁に接吻した。自分が百合から顔を離す拍子に思わず、遠い空を見たら、暁の星がたった一つ瞬いていた。
-        「百年はもう来ていたんだな」とこの時始めて気がついた。`,
-       icon: 'rabbit',
-       iconBackground: 'pink-blue'
-   },
-   {
-       id: 2,
-       name: 'ヒメコ',
-       //description: '縞々猫族の女の子。しましまのツインテールがチャームポイント。',
-       description: `　こんな夢を見た。
-       　和尚の室を退がって、廊下伝いに自分の部屋へ帰ると行灯がぼんやり点っている。片膝を座蒲団の上に突いて、灯心を掻き立てたとき、花のような丁子がぱたりと朱塗の台に落ちた。同時に部屋がぱっと明かるくなった。
-       　襖の画は蕪村の筆である。黒い柳を濃く薄く、遠近とかいて、寒むそうな漁夫が笠を傾けて土手の上を通る。床には海中文殊の軸が懸っている。焚き残した線香が暗い方でいまだに臭っている。広い寺だから森閑として、人気がない。黒い天井に差す丸行灯の丸い影が、仰向く途端に生きてるように見えた。
-       　立膝をしたまま、左の手で座蒲団を捲って、右を差し込んで見ると、思った所に、ちゃんとあった。あれば安心だから、蒲団をもとのごとく直して、その上にどっかり坐った。
-       　お前は侍である。侍なら悟れぬはずはなかろうと和尚が云った。そういつまでも悟れぬところをもって見ると、御前は侍ではあるまいと言った。人間の屑じゃと言った。ははあ怒ったなと云って笑った。口惜しければ悟った証拠を持って来いと云ってぷいと向をむいた。怪しからん。
-       　隣の広間の床に据えてある置時計が次の刻を打つまでには、きっと悟って見せる。悟った上で、今夜また入室する。そうして和尚の首と悟りと引替にしてやる。悟らなければ、和尚の命が取れない。どうしても悟らなければならない。自分は侍である。
-       　もし悟れなければ自刃する。侍が辱しめられて、生きている訳には行かない。綺麗に死んでしまう。
-       　こう考えた時、自分の手はまた思わず布団の下へ這入った。そうして朱鞘の短刀を引き摺り出した。ぐっと束を握って、赤い鞘を向へ払ったら、冷たい刃が一度に暗い部屋で光った。凄いものが手元から、すうすうと逃げて行くように思われる。そうして、ことごとく切先へ集まって、殺気を一点に籠めている。自分はこの鋭い刃が、無念にも針の頭のように縮められて、九寸五分の先へ来てやむをえず尖ってるのを見て、たちまちぐさりとやりたくなった。身体の血が右の手首の方へ流れて来て、握っている束がにちゃにちゃする。唇が顫えた。
-       　短刀を鞘へ収めて右脇へ引きつけておいて、それから全伽を組んだ。――趙州曰く無と。無とは何だ。糞坊主めとはがみをした。
-       　奥歯を強く咬み締めたので、鼻から熱い息が荒く出る。こめかみが釣って痛い。眼は普通の倍も大きく開けてやった。
-       　懸物が見える。行灯が見える。畳が見える。和尚の薬缶頭がありありと見える。鰐口を開いて嘲笑った声まで聞える。怪しからん坊主だ。どうしてもあの薬缶を首にしなくてはならん。悟ってやる。無だ、無だと舌の根で念じた。無だと云うのにやっぱり線香の香がした。何だ線香のくせに。
-       　自分はいきなり拳骨を固めて自分の頭をいやと云うほど擲った。そうして奥歯をぎりぎりと噛んだ。両腋から汗が出る。背中が棒のようになった。膝の接目が急に痛くなった。膝が折れたってどうあるものかと思った。けれども痛い。苦しい。無はなかなか出て来ない。出て来ると思うとすぐ痛くなる。腹が立つ。無念になる。非常に口惜しくなる。涙がほろほろ出る。ひと思に身を巨巌の上にぶつけて、骨も肉もめちゃめちゃに砕いてしまいたくなる。
-       　それでも我慢してじっと坐っていた。堪えがたいほど切ないものを胸に盛れて忍んでいた。その切ないものが身体中の筋肉を下から持上げて、毛穴から外へ吹き出よう吹き出ようと焦るけれども、どこも一面に塞がって、まるで出口がないような残刻極まる状態であった。
-       　そのうちに頭が変になった。行灯も蕪村の画も、畳も、違棚も有って無いような、無くって有るように見えた。と云って無はちっとも現前しない。ただ好加減に坐っていたようである。ところへ忽然隣座敷の時計がチーンと鳴り始めた。
-       　はっと思った。右の手をすぐ短刀にかけた。時計が二つ目をチーンと打った。`,
-       icon: 'cat',
-       iconBackground: 'pink-blue'
-   },
-   {
-    id: 3,
-    name: 'ハッチン',
-    description: 'Sting it to your heard!!',
-    icon: 'tropical',
-    iconBackground: 'pink-blue'
-}
-]
-
-var messages = [
-    {
-        id:1,
-        userId: 1,
-        message: 'Hello'
-    },
-    {
-        id: 2,
-        userId: myId,
-        message: 'こんにちは'
-    },
-    {
-        id: 3,
-        userId: 3,
-        message: 'Bonjour'
-    },
-    {
-        id: 4,
-        userId: 2,
-        message: '你早\nHola!'
-    },
-/*     {
-        id:5,
-        userId: 1,
-        message: `こんな夢を見た。
-        　腕組をして枕元に坐っていると、仰向に寝た女が、静かな声でもう死にますと云う。女は長い髪を枕に敷いて、輪郭の柔らかな瓜実顔をその中に横たえている。真白な頬の底に温かい血の色がほどよく差して、唇の色は無論赤い。とうてい死にそうには見えない。しかし女は静かな声で、もう死にますと判然云った。自分も確にこれは死ぬなと思った。そこで、そうかね、もう死ぬのかね、と上から覗き込むようにして聞いて見た。死にますとも、と云いながら、女はぱっちりと眼を開あけた。大きな潤のある眼で、長い睫に包まれた中は、ただ一面に真黒であった。その真黒な眸の奥に、自分の姿が鮮に浮かんでいる。
-        　自分は透き徹るほど深く見えるこの黒眼の色沢を眺めて、これでも死ぬのかと思った。それで、ねんごろに枕の傍へ口を付けて、死ぬんじゃなかろうね、大丈夫だろうね、とまた聞き返した。すると女は黒い眼を眠そうにみはったまま、やっぱり静かな声で、でも、死ぬんですもの、仕方がないわと云った。
-        　じゃ、私の顔が見えるかいと一心に聞くと、見えるかいって、そら、そこに、写ってるじゃありませんかと、にこりと笑って見せた。自分は黙って、顔を枕から離した。腕組をしながら、どうしても死ぬのかなと思った。
-        　しばらくして、女がまたこう云った。
-        「死んだら、埋めて下さい。大きな真珠貝で穴を掘って。そうして天から落ちて来る星の破片を墓標に置いて下さい。そうして墓の傍に待っていて下さい。また逢いに来ますから」
-        　自分は、いつ逢いに来るかねと聞いた。
-        「日が出るでしょう。それから日が沈むでしょう。それからまた出るでしょう、そうしてまた沈むでしょう。――赤い日が東から西へ、東から西へと落ちて行くうちに、――あなた、待っていられますか」
-        　自分は黙って首肯いた。女は静かな調子を一段張り上げて、
-        「百年待っていて下さい」と思い切った声で云った。
-        「百年、私の墓の傍に坐って待っていて下さい。きっと逢いに来ますから」
-        　自分はただ待っていると答えた。すると、黒い眸のなかに鮮に見えた自分の姿が、ぼうっと崩れて来た。静かな水が動いて写る影を乱したように、流れ出したと思ったら、女の眼がぱちりと閉じた。長い睫の間から涙が頬へ垂れた。――もう死んでいた。
-        　自分はそれから庭へ下りて、真珠貝で穴を掘った。真珠貝は大きな滑かな縁ふちの鋭どい貝であった。土をすくうたびに、貝の裏に月の光が差してきらきらした。湿った土の匂もした。穴はしばらくして掘れた。女をその中に入れた。そうして柔らかい土を、上からそっと掛けた。掛けるたびに真珠貝の裏に月の光が差した。
-        　それから星の破片の落ちたのを拾って来て、かろく土の上へ乗せた。星の破片は丸かった。長い間大空を落ちている間に、角が取れて滑かになったんだろうと思った。抱き上げて土の上へ置くうちに、自分の胸と手が少し暖くなった。
-        　自分は苔の上に坐った。これから百年の間こうして待っているんだなと考えながら、腕組をして、丸い墓石を眺めていた。そのうちに、女の云った通り日が東から出た。大きな赤い日であった。それがまた女の云った通り、やがて西へ落ちた。赤いまんまでのっと落ちて行った。一つと自分は勘定した。
-        　しばらくするとまた唐紅の天道がのそりと上って来た。そうして黙って沈んでしまった。二つとまた勘定した。
-        　自分はこう云う風に一つ二つと勘定して行くうちに、赤い日をいくつ見たか分らない。勘定しても、勘定しても、しつくせないほど赤い日が頭の上を通り越して行った。それでも百年がまだ来ない。しまいには、苔の生えた丸い石を眺めて、自分は女に欺されたのではなかろうかと思い出した。
-        　すると石の下から斜に自分の方へ向いて青い茎が伸びて来た。見る間に長くなってちょうど自分の胸のあたりまで来て留まった。と思うと、すらりと揺ぐ茎の頂に、心持首を傾けていた細長い一輪の蕾が、ふっくらと弁を開いた。真白な百合が鼻の先で骨に徹えるほど匂った。そこへ遥の上から、ぽたりと露が落ちたので、花は自分の重みでふらふらと動いた。自分は首を前へ出して冷たい露の滴る、白い花弁に接吻した。自分が百合から顔を離す拍子に思わず、遠い空を見たら、暁の星がたった一つ瞬いていた。
-        「百年はもう来ていたんだな」とこの時始めて気がついた。`
-    }, */
-    {
-        id:5,
-        userId: myId,
-        message: "Hello Hello New Era!"
-    },
-]
 
 var iconBackgrounds = [
     "pink-blue",
@@ -139,11 +18,15 @@ var iconBackgrounds = [
     
 ]
 
-//擬似的にWebAPI経由で情報を取得したようにする
-var getMessages = function(callback) {
-    setTimeout(function() {
-        callback(null, messages)
-    }, 1000)
+//WebAPI経由で情報を取得したようにする
+var getMessages = function(axios, callback) {
+    axios.get('/messages')
+         .then(response => {
+            callback(null, response.data);
+         })
+         .catch(error => {
+            callback(error, null)
+         })
 }
 var getUser = function(axios, userId, callback) {
     setTimeout(function() {
@@ -167,27 +50,49 @@ var getUsers = function(axios, callback) {
              }) 
     }, 1000) 
 }
+
+/*
+
+未実装
+
+*/
 var getIconBackgrounds = function(callback) {
     setTimeout(function() {
         callback(null, iconBackgrounds)
     }, 1000)
 }
-//擬似的にAPI経由で情報を更新したようにする
+//API経由で情報を更新したようにする
 var postMsg = function(params, callback) {
-    setTimeout(function() {
-        params.id = messages.length + 1
-        params.userId = myId
-        params.icon = myIcon
-        messages.push(params)
-        callback(null, params)
-    }, 10)
+    const axiosApi = axios.create({
+        headers: {
+            /* JSON形式にすると何故か失敗するのでtext形式で送る */
+            'Content-Type':'text/plain;charset=utf-8'
+        }
+    }) 
+
+    axiosApi.post("/addMessage", params)
+            .then(response => {
+                callback(null, response.data[0]);
+            })
+            .catch(error => {
+                callback(error, null)
+            }) 
 }
-var postUser = function(params, callback) {
-    setTimeout(function() {
-        params.id = userData.length + 1
-        userData.push(params)
-        callback(null, params)
-    }, 1000)
+var postUser = function(to, params, callback) {
+    const axiosApi = axios.create({
+        headers: {
+            /* JSON形式にすると何故か失敗するのでtext形式で送る */
+            'Content-Type':'text/plain;charset=utf-8'
+        }
+    }) 
+
+    axiosApi.post(to, params)
+            .then(response => {
+                callback(null, response.data);
+            })
+            .catch(error => {
+                callback(error, null)
+            }) 
 }
 
 var Messages = {
@@ -200,7 +105,8 @@ var Messages = {
             message: this.defaultMessage(),
             error: null,
             isFirst: false,
-            composing: false
+            composing: false,
+            users: function(){return []}, //初期値の空配列
         }
     },
     //初期化時にデータを取得
@@ -221,46 +127,71 @@ var Messages = {
     methods: {
         fetchData: function() {
             this.loading = true
-            getMessages((function(err, messages) {
+            getUsers(this.$axios, (function(err, resUsers) {
                 this.loading = false
                 if (err) {
                     this.error = err.toString()
                 } else {
-                    this.messages = messages;
+                    this.users = resUsers;
+
+                    getMessages(this.$axios, (function(err, messages) {
+                        this.loading = false
+                        if (err) {
+                            this.error = err.toString()
+                        } else {
+                            this.messages = messages
+                        }
+                    }).bind(this))    
                 }
-            }).bind(this))
+            }
+            ).bind(this))
         },
         defaultMessage: function() {
             return {
                 id: '',
-                userId: '',
+                userId: myId,
                 message: ''
             }
         },
         createMessage: function(){
             this.isFirst = false
             var textarea = document.getElementById('textarea');
-            var messages = document.getElementById('messages');
+            var messagesArea = document.getElementById('messages');
             textarea.style.height = '24px'
-            messages.style.paddingBottom = '88px'
+            messagesArea.style.paddingBottom = '88px'
             if(this.message.message.trim() === ''){
                 return
             }
-            postMsg(this.message, (function(err, message) {
+            postMsg(this.message, (function(err, resMsg) {
                 this.sending = false
                 if(err) {
                     this.error = err.toString()
                 } else {
                     this.error = null
-                    this.message = this.defaultMessage()
-                    //ユーザー一覧に戻る
-                    this.$router.push('/top')  
+                    this.messages.push({
+                        id: resMsg.id,
+                        userId: resMsg.userId,
+                        message: resMsg.message,
+                    });
+                    this.message = this.defaultMessage() 
                 }
-            }).bind(this))
+            }).bind(this)) 
         },
 
-        iconColor: function(userId) {return iconColor(userId)},
-        iconBackground: function(userId) {return iconBackground(userId)},
+        iconColor: function(userId) {
+            if(this.users[0]) {
+                var selectUser = this.users.filter(user => user.id === userId)
+                return selectUser[0].icon
+            }
+            return null; 
+        },
+        iconBackground: function(userId) {
+            if(this.users[0]) {
+                var selectUser = this.users.filter(user => user.id === userId)
+                return selectUser[0].iconBackground
+            }
+            return null; 
+        },
 
         keyEnter: function(event) {
             if (event.keyCode !== 13) return
@@ -416,33 +347,6 @@ var UserDetail = {
         afterEnter(el) {
             el.style.transitionDelay = ''
         },
-
-        /* APIのリクエスト お試し */
-        requestApi() {
-            console.log("いってらっしゃい");
-            requestApinum++;
-            this.$axios
-            .get('/user/' + this.$route.params.userId)
-            .then(response => {
-                console.log("おかえり");
-                this.result = response.data;
-
-                /* response.data.forEach(element => {
-                    
-                    this.users.push({
-                        id: element.id * Math.pow(100, requestApinum),
-                        name: element.name,
-                        description: element.description,
-                        icon: element.icon,
-                        iconBackground: element.iconBackground
-                    }); 
-                    
-                })   */
-            })
-            .catch(error => {
-                this.result = error;
-            })    
-        }
     },
 }
 
@@ -476,6 +380,17 @@ var UserInfo = {
                     console.log(this.user);
                 }
             }).bind(this))
+            /*
+
+
+
+            未実装
+
+
+
+
+
+            */
             getIconBackgrounds((function(err, iconBackgrounds) {
                 this.loading = false
                 if (err) {
@@ -527,51 +442,45 @@ var UserInfo = {
             selectUser[0].iconBackground = color
             this.user.iconBackground = color;
 
-            const axiosApi = axios.create({
-                headers: {
-                    /* JSON形式にすると何故か失敗するのでtext形式で送る */
-                    'Content-Type':'text/plain;charset=utf-8'
+            postUser('/changeBackground', this.user, (function(err, message) {
+                if(err) {
+                    this.error = err.toString()
+                } else {
+                    this.error = null
                 }
-            }) 
-
-            axiosApi.post('/changeBackground', 
-                /* {
-                        id: this.user.id,
-                        name: this.user.name,
-                        description: this.user.description,
-                        icon: this.user.icon,
-                        iconBackground: this.user.iconBackground
-                    
-                } , */
-
-                this.user
-                
-            )
+            }).bind(this))
         },
         setDescription: function() {
             var selectUser = this.users.filter(user => user.id === myId)
-            selectUser[0].description = this.user.description;
 
-            const axiosApi = axios.create({
-                headers: {
-                    /* JSON形式にすると何故か失敗するのでtext形式で送る */
-                    'Content-Type':'text/plain;charset=utf-8'
-                }
-            }) 
+            if(selectUser[0].description !== this.user.description)
+            {
+                selectUser[0].description = this.user.description;
 
-            axiosApi.post('/changeDescription', 
-                /* {
-                        id: this.user.id,
-                        name: this.user.name,
-                        description: this.user.description,
-                        icon: this.user.icon,
-                        iconBackground: this.user.iconBackground
-                    
-                } , */
+                postUser('/changeDescription', this.user, (function(err, message) {
+                    if(err) {
+                        this.error = err.toString()
+                    } else {
+                        this.error = null
+                    }
+                }).bind(this))
+            }
+        },
+        setName: function() {
+            var selectUser = this.users.filter(user => user.id === myId)
 
-                this.user
-                
-            )
+            if(selectUser[0].name !== this.user.name)
+            {    
+                selectUser[0].name = this.user.name;
+
+                postUser('/changeName', this.user, (function(err, message) {
+                    if(err) {
+                        this.error = err.toString()
+                    } else {
+                        this.error = null
+                    }
+                }).bind(this))
+            }
         },
         changeUser: function(user) {
             myId = user
@@ -615,21 +524,7 @@ var router = new VueRouter({
         {
             path: '/users',   //URLの指定。ファイル名#/usersでアクセスできる。
             component: UserList,
-            /*
-            beforeEnter: function (to, from, next) {
-                //users?redirect=trueでアクセスした時だけリダイレクト
-                if(to.query.redirect === 'true') {
-                    next('/top')
-                } else {
-                    next ()
-                }
-            }
-            */
         },
-        /* {
-            path: '/users/new',
-            component: UserCreate
-        }, */
         {
             path: '/user/info',
             component: UserInfo
@@ -642,48 +537,7 @@ var router = new VueRouter({
     ] 
 })
 
-/*  usersにアクセスしたらリダイレクトする
-router.beforeEach(function (to, from, next) {
-    if(to.path === '/users') {
-        next('/top')
-    } else {
-        next()
-    }
-})
-*/
-
 var app = new Vue({
     el: '#app',
     router: router
 })
-
-/*
-var fixedElem = document.querySelector('.input-message-area');
-var inputElem = document.querySelector('#textarea');
- 
-inputElem.addEventListener('focus', function() {
-    var scrollTop  = window.scrollY;
-    var scrollLeft = window.scrollX;
- 
-    fixedElem.style.position = 'absolute';
-    fixedElem.style.bottom      = 0 + 'px';
-});
- 
-inputElem.addEventListener('blur', function() {
-    // キーボードが消える時間を考慮してsetTimeoutの処理をする。
-    setTimeout(function() {
-        fixedElem.style.position = '';
-        fixedElem.style.bottom      = '';
-    }, 200);
-});
-*/
-
-iconColor = function(userId) {
-    var user = userData.filter(user => user.id === userId)
-    return user[0].icon
-}
-iconBackground = function(userId) {
-    var user = userData.filter(user => user.id === userId)
-    return user[0].iconBackground
-}
-
